3b1598d583d33485657061e82af83bf30809a12e
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-09-03 12:09:16 -0300

    Decoder

--- src/main.py
+++ src/main.py
@@ -17,6 +17,29 @@ TRUTH_TABLE = {
     (1, 0, 0, 1): (1, 1, 1, 1, 0, 1, 1),  # 9
 }
 
+TRUTH_TABLE_OUT = {
+    (0, 1, 1, 1, 0, 1, 1, 1): (0, 0, 0, 0, 0),  # 0
+    (0, 0, 0, 1, 0, 0, 1, 0): (0, 0, 0, 0, 1),  # 1
+    (0, 1, 0, 1, 1, 1, 0, 1): (0, 0, 0, 1, 0),  # 2
+    (0, 1, 0, 1, 1, 0, 1, 1): (0, 0, 0, 1, 1),  # 3
+    (0, 0, 1, 1, 1, 0, 1, 0): (0, 0, 1, 0, 0),  # 4
+    (0, 1, 1, 0, 0, 1, 0, 1): (0, 0, 1, 0, 1),  # 5
+    (0, 1, 1, 0, 1, 1, 1, 1): (0, 0, 1, 1, 0),  # 6
+    (0, 1, 0, 1, 1, 0, 1, 0): (0, 0, 1, 1, 1),  # 7
+    (0, 1, 1, 1, 1, 1, 1, 1): (0, 1, 0, 0, 0),  # 8
+    (0, 1, 1, 1, 1, 0, 1, 1): (0, 1, 0, 0, 1),  # 9
+    (1, 1, 1, 1, 0, 1, 1, 1): (0, 1, 0, 1, 0),  # 10
+    (1, 0, 0, 1, 0, 0, 1, 0): (0, 1, 0, 1, 1),  # 11
+    (1, 1, 0, 1, 1, 1, 0, 1): (0, 1, 1, 0, 0),  # 12
+    (1, 1, 0, 1, 1, 0, 1, 1): (0, 1, 1, 0, 1),  # 13
+    (1, 0, 1, 1, 1, 0, 1, 0): (0, 1, 1, 1, 0),  # 14
+    (1, 1, 1, 0, 0, 1, 0, 1): (0, 1, 1, 1, 1),  # 15
+    (1, 1, 1, 0, 1, 1, 1, 1): (1, 0, 0, 0, 0),  # 16
+    (1, 1, 0, 1, 1, 0, 1, 0): (1, 0, 0, 0, 1),  # 17
+    (1, 1, 1, 1, 1, 1, 1, 1): (1, 0, 0, 1, 0),  # 18
+}
+
+
 
 def sum_of_products(table: dict[tuple: tuple]) -> tuple[str]:
 
@@ -35,9 +58,11 @@ def sum_of_products(table: dict[tuple: tuple]) -> tuple[str]:
 
 
 # debug
-print('\nLEDS TO BITS\n')
-sum_of_products(TRUTH_TABLE)
-print('\nBITS TO LEDS\n')
-sum_of_products(dict(zip(TRUTH_TABLE.values(),
-                         TRUTH_TABLE.keys())))
-print()
+# print('\nLEDS TO BITS\n')
+# sum_of_products(TRUTH_TABLE)
+# print('\nBITS TO LEDS\n')
+# sum_of_products(dict(zip(TRUTH_TABLE.values(),
+#                          TRUTH_TABLE.keys())))
+sum_of_products(TRUTH_TABLE_OUT)
+# print()
+
